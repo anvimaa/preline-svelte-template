e7536466d18600edfaf75cb21eb848d0df883c8e
anvimaa <anvimaa@gmail.com>
2024-05-03 12:18:44 +0100

    Add FormHero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
   import Footer1 from "../components/sections/footers/Footer1.svelte";
   import FloatiangHeader from "../components/sections/header/FloatiangHeader.svelte";
+  import HeaderMegaMenu from "../components/sections/header/HeaderMegaMenu.svelte";
   import Hero1 from "../components/sections/hero/Hero1.svelte";
   import Hero2 from "../components/sections/hero/Hero2.svelte";
   import Hero3 from "../components/sections/hero/Hero3.svelte";
+  import HeroForm1 from "../components/sections/hero/HeroForm1.svelte";
 </script>
 
 <svelte:head>
@@ -11,7 +13,8 @@
   <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<FloatiangHeader />
+<!-- <FloatiangHeader /> -->
+<HeaderMegaMenu />
 
 <!-- ========== MAIN CONTENT ========== -->
 <main id="content">
@@ -19,6 +22,7 @@
     <Hero1 />
     <Hero2 />
     <Hero3 />
+    <HeroForm1 />
   </div>
 </main>
 <!-- ========== END MAIN CONTENT ========== -->
